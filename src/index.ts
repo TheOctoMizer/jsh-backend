2c69d174b9f4a04f02560e6d03f2d8deb8f25204
Chyavan Shenoy <chyavandshenoy2318@gmail.com>
2025-03-19 20:10:19 +0530

    Fastify basic setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World");
+import { fastify } from "fastify";
+
+const server = fastify();
+
+server.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+server.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
